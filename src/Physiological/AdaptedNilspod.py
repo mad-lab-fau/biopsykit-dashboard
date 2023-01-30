75416742b0fcf10889c7ea8bff246abe771973ca
Simon Meske <simon.h.meske@fau.de>
2023-01-30 20:21:19 +0100

    Initial adaptation to read in zipFiles to SyncedSessions

--- src/Physiological/AdaptedNilspod.py
+++ src/Physiological/AdaptedNilspod.py
@@ -1,13 +1,15 @@
 from io import BytesIO
 from pathlib import Path
+from zipfile import ZipFile
 
 import nilspodlib
 import numpy as np
-from nilspodlib.dataset import split_into_sensor_data
+from nilspodlib import SyncedSession
+from nilspodlib.dataset import split_into_sensor_data, Dataset
 from nilspodlib.exceptions import LegacyWarning
 from nilspodlib.header import Header
 from nilspodlib.utils import path_t
-from typing import Dict, Optional, Tuple
+from typing import Dict, Optional, Tuple, Iterable
 from nilspodlib.legacy import find_conversion_function, legacy_support_check
 from nilspodlib.utils import read_binary_uint8, get_strict_version_from_header_bytes
 from packaging.version import Version
@@ -18,21 +20,28 @@ import warnings
 
 def get_header_and_data_bytes(bin_file: BytesIO) -> Tuple[np.ndarray, np.ndarray]:
     """Separate a binary file into its header and data part."""
-    header_size = np.frombuffer(bin_file.read(1), dtype=np.dtype('B'))
-    header = np.append(header_size, np.frombuffer(bin_file.read(header_size[0] - 1), dtype=np.dtype('B')))
+    header_size = np.frombuffer(bin_file.read(1), dtype=np.dtype("B"))
+    header = np.append(
+        header_size,
+        np.frombuffer(bin_file.read(header_size[0] - 1), dtype=np.dtype("B")),
+    )
     data_bytes = np.frombuffer(bin_file.read(), dtype=np.dtype("B"))
     return header, data_bytes
 
 
 def parse_binary(
-        bin_file: BytesIO, legacy_support: str = "error", force_version: Optional[Version] = None,
-        tz: Optional[str] = None
+    bin_file: BytesIO,
+    legacy_support: str = "error",
+    force_version: Optional[Version] = None,
+    tz: Optional[str] = None,
 ) -> Tuple[Dict[str, np.ndarray], np.ndarray, Header]:
     header_bytes, data_bytes = get_header_and_data_bytes(bin_file)
     version = get_strict_version_from_header_bytes(header_bytes)
     if legacy_support == "resolve":
         version = force_version or version
-        header_bytes, data_bytes = find_conversion_function(version, in_memory=True)(header_bytes, data_bytes)
+        header_bytes, data_bytes = find_conversion_function(version, in_memory=True)(
+            header_bytes, data_bytes
+        )
     elif legacy_support in ["error", "warn"]:
         legacy_support_check(version, as_warning=(legacy_support == "warn"))
     else:
@@ -47,7 +56,10 @@ def parse_binary(
 
     counter, sensor_data = split_into_sensor_data(data, session_header)
 
-    if session_header.strict_version_firmware >= Version("0.13.0") and len(counter) != session_header.n_samples:
+    if (
+        session_header.strict_version_firmware >= Version("0.13.0")
+        and len(counter) != session_header.n_samples
+    ):
         warnings.warn(
             "The number of samples in the dataset does not match the number indicated by the header. "
             "This might indicate a corrupted file",
@@ -58,28 +70,37 @@ def parse_binary(
 
 
 class NilsPodAdapted(nilspodlib.Dataset):
-    def __init__(self, sensor_data: Dict[str, np.ndarray], counter: np.ndarray, info: Header):
+    def __init__(
+        self, sensor_data: Dict[str, np.ndarray], counter: np.ndarray, info: Header
+    ):
         super().__init__(sensor_data, counter, info)
 
     @classmethod
     def from_bin_file(
-                        cls,
-                        filepath_or_buffer: path_t | BytesIO,
-                        *,
-                        legacy_support: str = "error",
-                        force_version: Optional[Version] = None,
-                        tz: Optional[str] = None,
-                    ) -> Self:
+        cls,
+        filepath_or_buffer: path_t | BytesIO,
+        *,
+        legacy_support: str = "error",
+        force_version: Optional[Version] = None,
+        tz: Optional[str] = None,
+    ) -> Self:
         if isinstance(filepath_or_buffer, BytesIO):
             sensor_data, counter, info = parse_binary(
-                bin_file=filepath_or_buffer, legacy_support=legacy_support, force_version=force_version, tz=tz
+                bin_file=filepath_or_buffer,
+                legacy_support=legacy_support,
+                force_version=force_version,
+                tz=tz,
             )
             s = nilspodlib.Dataset(sensor_data, counter, info)
             return s
         else:
             path = Path(filepath_or_buffer)
             if path.suffix != ".bin":
-                ValueError('Invalid file type! Only ".bin" files are supported not {}'.format(path))
+                ValueError(
+                    'Invalid file type! Only ".bin" files are supported not {}'.format(
+                        path
+                    )
+                )
 
             sensor_data, counter, info = parse_binary(
                 path, legacy_support=legacy_support, force_version=force_version, tz=tz
@@ -90,3 +111,52 @@ class NilsPodAdapted(nilspodlib.Dataset):
             return s
 
 
+class SyncedSessionAdapted(nilspodlib.SyncedSession):
+    def __init__(self, datasets: Iterable[Dataset]):
+        super.__init__(datasets)
+
+    @classmethod
+    def from_file_paths(
+        cls,
+        paths: Iterable[str],
+        zip: ZipFile,
+        legacy_support: str = "error",
+        force_version: Optional[Version] = None,
+        tz: Optional[str] = None,
+    ) -> Self:
+        ds = ()
+        with zip as archive:
+            for path in paths:
+                ds += NilsPodAdapted.from_bin_file(
+                    BytesIO(archive.read(path)),
+                    legacy_support=legacy_support,
+                    force_version=force_version,
+                    tz=tz,
+                )
+        return SyncedSession(ds)
+
+    @classmethod
+    def from_folder_path(
+        cls,
+        zip: ZipFile,
+        filter_pattern: str = "*.bin",
+        legacy_support: str = "error",
+        force_version: Optional[Version] = None,
+        tz: Optional[str] = None,
+    ) -> Self:
+        ds = list(
+            filter(lambda file: file.endswith(filter_pattern), zip.namelist())
+        )  # hier müssen noch alle Dateien gesucht werden
+        if not ds:
+            raise ValueError(
+                'No files matching "{}" where found in zipFile'.format(filter_pattern)
+            )
+        return SyncedSession(
+            cls.from_file_paths(
+                ds,
+                zip=zip,
+                legacy_support=legacy_support,
+                force_version=force_version,
+                tz=tz,
+            )
+        )
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,21 @@
 import warnings
 from datetime import datetime
-from typing import Tuple
+from io import BytesIO
+from typing import Tuple, Optional, Union, Sequence, Dict
+from zipfile import ZipFile
+
+from biopsykit.io.nilspod import _handle_counter_inconsistencies_session
+from biopsykit.utils._datatype_validation_helper import _assert_file_extension
+from biopsykit.utils.time import tz
+
 import panel as pn
 import biopsykit
 import numpy as np
 import pandas as pd
-from nilspodlib import Dataset
+from nilspodlib import Dataset, SyncedSession
 from typing_extensions import Literal
+
+from src.Physiological.AdaptedNilspod import SyncedSessionAdapted, NilsPodAdapted
 
 COUNTER_INCONSISTENCY_HANDLING = Literal["raise", "warn", "ignore"]
 
@@ -68,3 +77,104 @@ def _handle_counter_inconsistencies_dataset(
                 "using `biopsykit.io.nilspod.load_synced_session_nilspod()`. "
                 "Check the counter of the DataFrame manually!"
             )
+
+
+def load_synced_session_nilspod_zip(
+    file: ZipFile,
+    datastreams: Optional[Union[str, Sequence[str]]] = None,
+    handle_counter_inconsistency: Optional[COUNTER_INCONSISTENCY_HANDLING] = "raise",
+    **kwargs,
+) -> Tuple[pd.DataFrame, float]:
+
+    nilspod_files = list(
+        filter(lambda file_name: file_name.endswith(".bin"), file.namelist())
+    )
+    if len(nilspod_files) == 0:
+        raise ValueError("No NilsPod files found in Zipfile!")
+
+    kwargs.setdefault("tz", kwargs.pop("timezone", tz))
+    session = SyncedSessionAdapted.from_folder_path(file, **kwargs)
+
+    # Ab hier standard
+    session.align_to_syncregion(inplace=True)
+
+    _handle_counter_inconsistencies_session(session, handle_counter_inconsistency)
+    if isinstance(datastreams, str):
+        datastreams = [datastreams]
+
+    # convert dataset to dataframe and localize timestamp
+    df = session.data_as_df(datastreams, index="local_datetime", concat_df=True)
+    df.index.name = "time"
+    if len(set(session.info.sampling_rate_hz)) > 1:
+        raise ValueError(
+            f"Datasets in the sessions have different sampling rates! Got: {session.info.sampling_rate_hz}."
+        )
+    fs = session.info.sampling_rate_hz[0]
+    return df, fs
+
+
+def load_dataset_nilspod_zip(
+    zipFile: Optional[ZipFile] = None,
+    file_path: Optional[str] = None,
+    dataset: Optional[Dataset] = None,
+    datastreams: Optional[Union[str, Sequence[str]]] = None,
+    handle_counter_inconsistency: Optional[COUNTER_INCONSISTENCY_HANDLING] = "raise",
+    **kwargs,
+) -> Tuple[pd.DataFrame, float]:
+    if zipFile is not None:
+        _assert_file_extension(file_path, ".bin")
+        kwargs.setdefault("tz", kwargs.pop("timezone", tz))
+        kwargs.setdefault("legacy_support", "resolve")
+        dataset = NilsPodAdapted.from_bin_file(
+            BytesIO(zipFile.read(file_path)), **kwargs
+        )
+
+    if file_path is None and dataset is None:
+        raise ValueError(
+            "Either 'file_path' or 'dataset' must be supplied as parameter!"
+        )
+
+    _handle_counter_inconsistencies_dataset(dataset, handle_counter_inconsistency)
+
+    if isinstance(datastreams, str):
+        datastreams = [datastreams]
+
+    # convert dataset to dataframe and localize timestamp
+    df = dataset.data_as_df(datastreams, index="local_datetime")
+    df.index.name = "time"
+    return df, dataset.info.sampling_rate_hz
+
+
+def load_folder_nilspod_zip(
+    file: ZipFile, phase_names: Optional[Sequence[str]] = None, **kwargs
+) -> Tuple[Dict[str, pd.DataFrame], float]:
+    dataset_list = list(
+        filter(lambda file_name: file_name.endswith(".bin"), file.namelist())
+    )
+    if len(dataset_list) == 0:
+        raise ValueError(f"No NilsPod files found in Zipfile!")
+    if phase_names is None:
+        phase_names = [f"Part{i}" for i in range(len(dataset_list))]
+
+    if len(phase_names) != len(dataset_list):
+        raise ValueError(
+            f"Number of phases does not match number of datasets in the folder! "
+            f"Expected {len(dataset_list)}, got {len(phase_names)}."
+        )
+
+    dataset_list = [
+        load_dataset_nilspod_zip(file_path=dataset_path, zipFile=file, **kwargs)
+        for dataset_path in dataset_list
+    ]
+
+    # check if sampling rate is equal for all datasets in folder
+    fs_list = [fs for df, fs in dataset_list]
+
+    if len(set(fs_list)) > 1:
+        raise ValueError(
+            f"Datasets in the sessions have different sampling rates! Got: {fs_list}."
+        )
+    fs = fs_list[0]
+
+    dataset_dict = {phase: df for phase, (df, fs) in zip(phase_names, dataset_list)}
+    return dataset_dict, fs
